b5551d5a24ee545fb7ad2f0b632a20c5d5d4cc52
VikramShawFL <vikram@front.live>
2019-02-28 14:27:44 +0530

    600 Rows and 400 column

--- src/ppm_project/core.clj
+++ src/ppm_project/core.clj
@@ -1,14 +1,14 @@
 (ns ppm-project.core)
 
 (def foo
-  (let [x     200
-        y     200
+  (let [x     600
+        y     400
         head (str "P3\n" x " " y "\n255\n")
         matrix   (clojure.string/join (for [j (range (dec y) -1 -1)
                                           i (range 0 x)
                                           :let  [r (/ i x)
                                                  g (/ j y)
-                                                 b 0.6
+                                                 b 0.7
                                                  ir (int (* 255.99 r))
                                                  ig (int (* 255.99 g))
                                                  ib (int (* 255.99 b))]]
